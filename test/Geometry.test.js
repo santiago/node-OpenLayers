b6d234f7e0c26e4512646132c309eca7a1854f2b
santiago <sgaviria@gmail.com>
2011-07-19 13:52:00 -0500

    ported BaseTypes; LineString; LinearRing

--- test/Geometry.test.js
+++ test/Geometry.test.js
@@ -2,24 +2,25 @@ var OpenLayers= require('../lib/OpenLayers');
 var assert    = require('assert');
 
 var curve;
+var line_string;
 
 module.exports = {
     /*
      * Point
      * ======
      */
-    "<Point> it should load Point": function(done) {
+    "<Point>": function(done) {
 	var point= new OpenLayers.Geometry.Point(0,0);
 	done();
     },
-    "<Point#distanceTo> it should calculate distance between two points": function(done) {
+    "<Point#distanceTo>": function(done) {
 	var point_A= new OpenLayers.Geometry.Point(0,0);
 	var point_B= new OpenLayers.Geometry.Point(2,1);
 	var d= point_A.distanceTo(point_B);
 	// console.log(d);
 	done();
     },
-    "<Point#getCentroid> it should calculate centroid": function(done) {
+    "<Point#getCentroid>": function(done) {
 	var point= new OpenLayers.Geometry.Point(0,0);
 	var centroid= point.getCentroid();
 	// console.log(centroid);
@@ -54,9 +55,10 @@ module.exports = {
 	var points= [ 
 	    new OpenLayers.Geometry.Point(0,0),
 	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,0)
 	];
 	var multipoint= new OpenLayers.Geometry.MultiPoint(points);
-	multipoint.removePoint(new OpenLayers.Geometry.Point(1,0));
+	multipoint.removePoint(points[2]);
 	// console.log(multipoint.components);
 	done();
     },
@@ -80,6 +82,162 @@ module.exports = {
 	// should be 3*sqrt(2) and it is :)
 	// console.log(length);
 	done();
+    },
+
+    /*
+     * LineString
+     * ===========
+     */
+    "<LineString>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,0),
+	    new OpenLayers.Geometry.Point(1,1)
+	];
+	line_string= new OpenLayers.Geometry.LineString(points);
+	done();
+    },
+    "<LineString#removeComponent>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,0),
+	    new OpenLayers.Geometry.Point(1,1)
+	];
+	line_string= new OpenLayers.Geometry.LineString(points);
+	line_string.removeComponent(points[3]);
+	// console.log(line_string.components);
+	done();
+    },
+    "<LineString#intersects>": function(done) {
+	var points_A= [ 
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(2,1)
+	];
+	var points_B= [ 
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(1,0)
+	];
+	var line_A= new OpenLayers.Geometry.LineString(points_A);
+	var line_B= new OpenLayers.Geometry.LineString(points_B);
+	// console.log(line_A.intersects(line_B));
+	done();
+    },
+    "<LineString#distanceTo>": function(done) {
+	var points_A= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(1,0)
+	];
+	var points_B= [ 
+	    new OpenLayers.Geometry.Point(2,1),
+	    new OpenLayers.Geometry.Point(3,0)
+	];
+	var line_A= new OpenLayers.Geometry.LineString(points_A);
+	var line_B= new OpenLayers.Geometry.LineString(points_B);
+	console.log(line_A.distanceTo(line_B, {details:true}));
+	done();
+    },
+
+    /*
+     * LinearRing
+     * ===========
+     */
+    "<LinearRing>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,0),
+	    new OpenLayers.Geometry.Point(1,1)
+	];
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	done();
+    },
+    "<LinearRing#getCentroid>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(1,0)
+
+	];
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	// console.log(linear_ring.getCentroid());
+	done();
+    },
+    "<LinearRing#getArea>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(1,0)
+
+	];
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	// console.log(linear_ring.getArea());
+	done();
+    },
+
+    /*
+     * Polygon
+     * ========
+     */
+    "<Polygon>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(1,0)
+	];    
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	var polygon= new OpenLayers.Geometry.Polygon(linear_ring);
+	done();
+    },
+    "<Polygon#getArea>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(2,0)
+	];    
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	var polygon= new OpenLayers.Geometry.Polygon(linear_ring);
+	// console.log(polygon.getArea());
+	done();
+    },
+    "<Polygon#getCentroid>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(2,0)
+	];    
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	var polygon= new OpenLayers.Geometry.Polygon(linear_ring);
+	// console.log(polygon.getCentroid());
+	done();
+    },
+    "<Polygon#containsPoint>": function(done) {
+	var points= [ 
+	    new OpenLayers.Geometry.Point(0,0),
+	    new OpenLayers.Geometry.Point(0,1),
+	    new OpenLayers.Geometry.Point(1,1),
+	    new OpenLayers.Geometry.Point(2,0)
+	];    
+	var point= new OpenLayers.Geometry.Point(1,0.5);
+	var linear_ring= new OpenLayers.Geometry.LinearRing(points);
+	var polygon= new OpenLayers.Geometry.Polygon(linear_ring);
+	console.log(polygon.containsPoint(point));
+	done();
+    },
+    "<Polygon.createRegularPolygon>": function(done) {
+	var origin= new OpenLayers.Geometry.Point(20,20);
+	var radius= 2;
+	var sides= 20; // Approximates a circle
+	var rotation= 0;
+	var polygon= OpenLayers.Geometry.Polygon
+	    .createRegularPolygon(origin, radius, sides, rotation);
+	// console.log(polygon);
+	done();
     }
-	
 };
